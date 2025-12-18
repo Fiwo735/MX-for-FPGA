090011d95379199f3f7d276f5b2545ff8e1e8cfa
faw18 <faw18@ic.ac.uk>
2025-12-18 16:57:49 +0000

    Added -retiming flag back

--- src/attention/attention_int/run_synth.tcl
+++ src/attention/attention_int/run_synth.tcl
@@ -33,7 +33,7 @@ read_xdc        [ glob ./src/*.xdc ]
 # Suppress "Port unconnected" warnings (benign for parameterized IP and combinatorial wrappers)
 set_msg_config -id {Synth 8-7129} -suppress
 set t1 [clock milliseconds]
-synth_design -top $top -part $part -flatten rebuilt -generic $generics
+synth_design -top $top -part $part -flatten rebuilt -retiming -generic $generics
 set t2 [clock milliseconds]
 puts "Time for synth_design: [expr {($t2 - $t1) / 1000.0}] seconds"
 
@@ -46,12 +46,12 @@ puts "Time for write_checkpoint: [expr {($t3 - $t2) / 1000.0}] seconds"
 report_utilization      -file ${prefix}_util.rpt
 set t4 [clock milliseconds]
 puts "Time for report_utilization: [expr {($t4 - $t3) / 1000.0}] seconds"
-# report_timing_summary   -datasheet -file ${prefix}_timing.rpt
-# set t5 [clock milliseconds]
-# puts "Time for report_timing_summary: [expr {($t5 - $t4) / 1000.0}] seconds"
-# report_power            -file ${prefix}_power.rpt
-# set t6 [clock milliseconds]
-# puts "Time for report_power: [expr {($t6 - $t5) / 1000.0}] seconds"
+report_timing_summary   -datasheet -file ${prefix}_timing.rpt
+set t5 [clock milliseconds]
+puts "Time for report_timing_summary: [expr {($t5 - $t4) / 1000.0}] seconds"
+report_power            -file ${prefix}_power.rpt
+set t6 [clock milliseconds]
+puts "Time for report_power: [expr {($t6 - $t5) / 1000.0}] seconds"
 
 # opt_design
 # place_design
