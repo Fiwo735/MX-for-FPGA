3ae1a3f764133f73e5802c236f46e8b058ff0d4b
Ebby Samson <es1219@ee-kraken.ee.ic.ac.uk>
2026-02-12 18:07:40 +0000

    Removed unused files.

--- quant/quant_acc/ordmm.cu
+++ quant/quant_acc/ordmm.cu
@@ -1,8 +1,6 @@
 #include <torch/extension.h>
 #include "ordmm_chunk_bcast_scaled.cuh"
 #include "ordacc_chunk.cuh"
-#include "ordacc_sum_int.cuh"
-#include "ordmm_int.cuh"
 
 
 
@@ -10,6 +8,4 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     // Software emulation of accumulator quantization.
     m.def("ordmm_chunk_bcast_scaled", &ordmm_chunk_bcast_scaled, "ordmm_chunk_bcast_scaled");
     m.def("ordacc_chunk_scaled", &ordacc_chunk_scaled, "ordacc_chunk_scaled");
-    m.def("ordmm_chunk_bcast_scaled_int", &ordmm_chunk_bcast_scaled_int, "ordmm_chunk_bcast_scaled_int");
-    m.def("ordacc_chunk_scaled_int", &ordacc_chunk_scaled_int, "ordacc_chunk_scaled_int");
 }
